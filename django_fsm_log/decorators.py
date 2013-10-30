c6eff83d0c58ee5240ffeb1ed92c8775e3af2306
Jacob Haslehurst <jacob@haslehurst.net>
2013-10-30 19:56:30 +1100

    fsm_log_by decorator
    Automatically applies a 'by' attribute to the instance if there is a
    'by' kwarg on the transition method.

---
+++ django_fsm_log/decorators.py
@@ -0,0 +1,16 @@
+def fsm_log_by(func):
+    def wrapped(*args, **kwargs):
+        arg_list = list(args)
+        instance = arg_list.pop(0)
+
+        if kwargs.get('by', False):
+            instance.by = kwargs['by']
+
+        out = func(instance, *arg_list, **kwargs)
+
+        if kwargs.get('by', False):
+            delattr(instance, 'by')
+
+        return out
+
+    return wrapped
