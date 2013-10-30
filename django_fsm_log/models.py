628330f32cd6b83e99daddfd1a37dd4e5685c0e0
Jacob Haslehurst <jacob@haslehurst.net>
2013-10-30 17:53:08 +1100

    adding and connecting transition_callback

--- django_fsm_log/models.py
+++ django_fsm_log/models.py
@@ -5,6 +5,8 @@ from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from django_fsm.signals import post_transition
+
 try:
     from django.contrib.auth import get_user_model
 except ImportError: # django < 1.5
@@ -21,3 +23,16 @@ class StateLog(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField(db_index=True)
     content_object = GenericForeignKey('content_type', 'object_id')
+
+
+def transition_callback(sender, instance, name, source, target, **kwargs):
+    state_log = StateLog(
+        by=getattr(instance, 'by', None),
+        state=target,
+        transition=name,
+        content_object=instance,
+    )
+    state_log.save()
+
+
+post_transition.connect(transition_callback)
