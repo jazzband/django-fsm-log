60f77d2e878a5aaf1d7cd33181476a889b52c490
Jacob Haslehurst <jacob@haslehurst.net>
2013-11-04 14:38:38 +1100

    adding in initial test modules

---
+++ tests/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django_fsm.db.fields import FSMField, transition
+from django_fsm_log.decorators import fsm_log_by
+
+class Article(models.Model):
+    STATES = (
+        'draft',
+        'submitted',
+        'published',
+    )
+
+    state = FSMField(default='unborn', protected=True)
+
+    @fsm_log_by
+    @transition(field=state, source='draft', target='submitted')
+    def submit(self, by=None):
+        pass
+
+    @fsm_log_by
+    @transition(field=state, source='submitted', target='draft')
+    def request_changes(self, by=None):
+        pass
+
+    @fsm_log_by
+    @transition(field=state, source='submitted', target='published')
+    def publish(self, by=None):
+        pass
