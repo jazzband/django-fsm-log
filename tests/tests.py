a699a45fd5c9adfba94dab4acd5a6cecf0a5e5ef
Justin Arulnathan <justin@gizmag.com>
2014-01-22 15:45:53 +1100

    tests for pre_transition_callback and post_transition_callback

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django_fsm.db.fields import TransitionNotAllowed
-from django_fsm_log.models import StateLog
+from django_fsm_log.models import StateLog, post_transition_callback, pre_transition_callback
 from .models import Article
+from mock import patch
 
 try:
     from django.contrib.auth import get_user_model
@@ -61,6 +62,41 @@ class StateLogModelTests(TestCase):
 
         log = StateLog.objects.all()[0]
         self.assertEqual(log.content_object, self.article)
+
+    def test_get_pending_cache_key_for_object_returns_correctly_formatted_string(self):
+        expected_result = 'StateLog:{}:{}'.format(
+            self.article.__class__.__name__,
+            self.article.pk
+        )
+        result = StateLog.get_pending_cache_key_for_object(self.article)
+        self.assertEqual(result, expected_result)
+
+    @patch('django_fsm_log.models.cache')
+    def test_pre_transition_callback__pending_state_log(self, mock_cache):
+        cache_key = StateLog.get_pending_cache_key_for_object(self.article)
+        pre_transition_callback(self.article, self.article, 'mytransition', 'mysource', 'mytarget')
+        expected_cache_object = StateLog.objects.create(
+            by=None,
+            state='mytarget',
+            transition='mytransition',
+            content_object=self.article
+        )
+        cache_key = mock_cache.set.call_args_list[0][0][0]
+        cache_object = mock_cache.set.call_args_list[0][0][1]
+
+        self.assertEqual(cache_key, StateLog.get_pending_cache_key_for_object(self.article))
+        self.assertEqual(cache_object.state, expected_cache_object.state)
+        self.assertEqual(cache_object.transition, expected_cache_object.transition)
+        self.assertEqual(cache_object.content_object, expected_cache_object.content_object)
+
+
+    @patch('django_fsm_log.models.cache')
+    def test_post_transition_callback_gets_and_deletes_pending_state_log(self, mock_cache):
+        cache_key = StateLog.get_pending_cache_key_for_object(self.article)
+        post_transition_callback(self.article, self.article, 'submit', 'unfinished', 'submitted')
+        mock_cache.get.assert_called_once_with(cache_key)
+        mock_cache.delete.assert_called_once_with(cache_key)
+
 
 
 class StateLogManagerTests(TestCase):
