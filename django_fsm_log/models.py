d8c6d13950eca8dedec045d9ef6b6ecd6102b2a7
Justin Arulnathan <justin@gizmag.com>
2014-02-04 13:13:07 +1100

    renamed settings name

--- django_fsm_log/models.py
+++ django_fsm_log/models.py
@@ -31,7 +31,7 @@ class StateLog(models.Model):
             self.transition
         )
 
-backend = import_class_by_path(settings.DJANGO_FSM_LOG_CACHE_BACKEND)
+backend = import_class_by_path(settings.DJANGO_FSM_LOG_STORAGE_METHOD)
 backend.setup_model(StateLog)
 pre_transition.connect(backend.pre_transition_callback)
 post_transition.connect(backend.post_transition_callback)
