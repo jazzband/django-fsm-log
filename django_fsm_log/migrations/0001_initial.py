3e50e5fb0aec91d9c98ba142d41312978220bdb3
Jacob Haslehurst <jacob@haslehurst.net>
2014-08-06 14:44:19 +1000

    create 1.7 migration

---
+++ django_fsm_log/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='StateLog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('state', models.CharField(max_length=255, db_index=True)),
+                ('transition', models.CharField(max_length=255)),
+                ('object_id', models.PositiveIntegerField(db_index=True)),
+                ('by', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
