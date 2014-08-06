408707df7ffb006b080a42f524b478b63eaa0fb7
Jacob Haslehurst <jacob@haslehurst.net>
2014-08-06 15:01:14 +1000

    drop in app config boilerplate

---
+++ apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+from django.apps import AppConfig
+
+
+class DjangoFSMLogAppConfig(AppConfig):
+    name = 'django_fsm_log'
+    verbose_name = "Django FSM Log"
