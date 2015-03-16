8f8dcee6806264d64db69590c683801b3d256e2b
Jacob Haslehurst <jacob@haslehurst.net>
2015-03-16 14:09:24 +1100

    bump to v1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-fsm-log',
-    version='1.1.0',
+    version='1.2.0',
     description='Logging for django-fsm',
     author='Gizmag',
     author_email='tech@gizmag.com',
