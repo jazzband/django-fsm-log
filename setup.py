861c9a86021926a36b9c1835a13224577c7fb50c
Aly Sivji <alysivji@gmail.com>
2020-01-16 18:39:36 -0600

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readfile(filename):
 
 setup(
     name='django-fsm-log',
-    version='1.6.2',
+    version='1.7.0',
     description='Logging for django-fsm',
     long_description=readfile('README.md'),
     long_description_content_type='text/markdown',
